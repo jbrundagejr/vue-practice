c4dc09873e3f6dd187cfc3b15d1dbee4b8180f9d
Jon <jbrundagejr@gmail.com>
2022-02-26 11:02:49 -0500

    slots, props, conditional classes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+      </template>
+      <h1>YO YO YO</h1>
+      <p>This is a slot</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "A New Vue App",
+      header: "Sign up!",
+      text: "Grab your shit!",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
